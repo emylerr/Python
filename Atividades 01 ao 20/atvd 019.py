3f35c75ab1e6cd9fcb352d5ad2e234dfbee62540
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-07 15:51:07 -0300

    Python

---
+++ Atividades 01 ao 20/atvd 019.py
@@ -0,0 +1,4 @@
+salario = float(input('digite o salário do funcionário: R$'))
+aument = salario * 0.15
+res = salario + aument
+print(f'o salário atual do funcionário é de {salario}, com o aumento de 15% ele ficará {res:.2f}')
