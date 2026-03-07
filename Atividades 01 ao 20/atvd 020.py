fb0a7fa4b2bae913527c16c789ef29e98370c894
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-07 20:49:18 -0300

    Python

---
+++ Atividades 01 ao 20/atvd 020.py
@@ -0,0 +1,4 @@
+'''convertendo graus C° para Fahrenheit'''
+grau = float(input('digite a temperatura em graus C°: '))
+fahr = (grau * 1.8) + 32
+print(f'a temperatura {grau}C° convertida para Fahrenheit é {fahr}')
