0a899f4ae8c98a8683b6945b853e18488ace7f69
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-03-07 13:02:00 -0300

    Python

--- atvd 005.py
+++ atvd 005.py
@@ -1,8 +1,8 @@
-val = int (input('digite um valor em metros: '))
+val = float(input('digite um valor em metros: '))
 km = val / 1000
 hm = val / 100
 dam = val / 10
 dm = val * 10
 cm = val * 100
 mm = val * 1000
-print(f'{val} em metros é igual a {km}km, {hm}hm, {dam}dam, {cm}cm, {dm}dm e {mm}mm')
+print(f'{val}m é igual a: \n km: {km} \n hm: {hm} \n dam: {dam} \n cm: {cm} \n dm: {dm} \n mm: {mm}')
